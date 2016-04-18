62b61bd943b6f854ac624fa56f3b1f3967977ea0
John Oksasoglu <john.oksasoglu@gmail.com>
2016-04-18 10:15:17 -0700

    Wire up passing user data props to cells

--- src/LeaderboardItem.js
+++ src/LeaderboardItem.js
@@ -3,17 +3,31 @@ import CSSModules from "react-css-modules";
 import styles from "./styles/leaderboardItem.scss";
 import LeaderboardCell from "./LeaderboardCell";
 
+// Should receive props in form of an object that contains all 4 pieces of data
+// Plus a url to that user's profile image
+// Props.children for the user cell should be composed of another component?
 class LeaderboardItem extends React.Component {
+  // Define what components are received
   render () {
     return (
       <div styleName="item">
-      <LeaderboardCell style="rank">Rank</LeaderboardCell>
-      <LeaderboardCell style="user">User</LeaderboardCell>
-      <LeaderboardCell style="thirty">30 Days</LeaderboardCell>
-      <LeaderboardCell style="alltime">All-time</LeaderboardCell>
+        <LeaderboardCell style="rank">{this.props.data.rank}</LeaderboardCell>
+        <LeaderboardCell style="user">{this.props.data.username}</LeaderboardCell>
+        <LeaderboardCell style="thirty">{this.props.data.thirty}</LeaderboardCell>
+        <LeaderboardCell style="alltime">{this.props.data.alltime}</LeaderboardCell>
       </div>
     );
   }
 }
+
+LeaderboardItem.propTypes = {
+  data: React.PropTypes.shape({
+    rank: React.PropTypes.number.isRequired,
+    username: React.PropTypes.string.isRequired,
+    picture: React.PropTypes.string.isRequired,
+    thirty: React.PropTypes.number.isRequired,
+    alltime: React.PropTypes.number.isRequired
+  }).isRequired
+};
 
 export default CSSModules(LeaderboardItem, styles);
--- src/LeaderboardList.js
+++ src/LeaderboardList.js
@@ -3,11 +3,25 @@ import CSSModules from "react-css-modules";
 import styles from "./styles/leaderboardList.scss";
 import LeaderboardItem from "./LeaderboardItem";
 
+// Is a smart component? Is this where the requests etc are made?
+
+var sample = {
+    rank: 1,
+    username: "quincylarson",
+    picture: "some url",
+    thirty: 40,
+    alltime: 5000
+};
+
 class LeaderboardList extends React.Component {
   render () {
     return (
       <div>
-        <LeaderboardItem/>
+        <LeaderboardItem data={sample} />
+        <LeaderboardItem data={sample} />
+        <LeaderboardItem data={sample} />
+        <LeaderboardItem data={sample} />
+        <LeaderboardItem data={sample} />
       </div>
     );
   }
