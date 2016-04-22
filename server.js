e659542b1aad82f7e299f95555a0a0b435dd9a85
John Oksasoglu <john.oksasoglu@gmail.com>
2016-04-22 04:08:39 +0000

    Update server.js with variable address

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var app = express();
 
 var port = process.env.PORT || 4040;
+var address = process.env.IP || "localhost";
 
 app.use(express.static(__dirname));
 
@@ -10,7 +11,7 @@ app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(port, "localhost", function(err) {
+app.listen(port, address, function(err) {
   if (err) {
     return console.error(err);
   }
